62bc8e4b0088abbf463d7a61b855ec3359517433
Long Bui <86465921+longbradbui@users.noreply.github.com>
2024-01-28 11:04:12 -0800

    Update complex_number.h

--- complex_number.h
+++ complex_number.h
@@ -10,14 +10,27 @@ public:
     ComplexNumber(double real, double imaginary);
     double real() const;
     double imaginary() const;
-    ComplexNumber operator*=(ComplexNumber const& rhs);
-    ComplexNumber operator*(const ComplexNumber& rhs) const;
-    friend ostream& operator<<(ostream& ostream, const ComplexNumber& rhs);
-    //friend istream& operator>>(ostream& istream, ComplexNumber& rhs);
+    double magnitude() const;
+    friend ostream &operator<<(ostream &ostream, const ComplexNumber &rhs);
+    friend istream &operator>>(istream &istream, ComplexNumber &rhs);
+    ComplexNumber &operator+=(ComplexNumber const &rhs);
+    ComplexNumber &operator-=(ComplexNumber const &rhs);
+    ComplexNumber &operator*=(ComplexNumber const &rhs);
+    ComplexNumber &operator/=(ComplexNumber const &rhs);
+    ComplexNumber operator+(const ComplexNumber &rhs) const;
+    ComplexNumber operator-(const ComplexNumber &rhs) const;
+    ComplexNumber operator*(const ComplexNumber &rhs) const;
+    ComplexNumber operator/(const ComplexNumber &rhs) const;
+    bool operator<(const ComplexNumber &rhs) const;
+    bool operator>(const ComplexNumber &rhs) const;
+    bool operator<=(const ComplexNumber &rhs) const;
+    bool operator>=(const ComplexNumber &rhs) const;
+    bool operator==(const ComplexNumber &rhs) const;
+    bool operator!=(const ComplexNumber &rhs) const;
+
 private:
     double real_;
     double imaginary_;
-
+    double magnitude_;
 };
 #endif
-
